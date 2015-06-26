996a1d7c64c848c18eff53478b622cb1680c5193
8BitYoda <htnitram@gmail.com>
2015-06-26 13:43:47 -0400

    added expandable card to Education

--- app/src/main/java/com/example/martin/resume2015/Education.java
+++ app/src/main/java/com/example/martin/resume2015/Education.java
@@ -1,19 +1,27 @@
 package com.example.martin.resume2015;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 public class Education extends Fragment {
-    private View rootView;
+//    View rootView;
+    LinearLayout mLinearLayout;
+    LinearLayout mLinearLayoutHeader;
+    ValueAnimator mAnimator;
+    ImageView expndIcon;
 
     public static Education newInstance(Context context) {
-        Education fragment = new Education();
-        return fragment;
+        return new Education();
     }
 
     public Education() {
@@ -29,10 +37,99 @@ public class Education extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        if(container == null) return null;
+
         ((MainActivity) getActivity()).setActionBarTitle("Education"); //changes title
 
         // Inflate the layout for this fragment
-        rootView = inflater.inflate(R.layout.fragment_education, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_education, container, false);
+        mLinearLayout = (LinearLayout) rootView.findViewById(R.id.csu_expandable);
+        mLinearLayoutHeader = (LinearLayout) rootView.findViewById(R.id.csu_header);
+        expndIcon = (ImageView) rootView.findViewById(R.id.expand_details);
+
+        //sets up and handles cardview expand
+        mLinearLayout.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        mLinearLayout.getViewTreeObserver().removeOnPreDrawListener(this);
+                        mLinearLayout.setVisibility(View.GONE);
+
+                        final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                        final int heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                        mLinearLayout.measure(widthSpec, heightSpec);
+
+                        mAnimator = slideAnimator(0, mLinearLayout.getMeasuredHeight());
+                        return true;
+                    }
+                });
+
+        mLinearLayoutHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mLinearLayout.getVisibility()==View.GONE){
+                    expndIcon.setImageResource(R.drawable.up);
+                    expand();
+                }else{
+                    expndIcon.setImageResource(R.drawable.down);
+                    collapse();
+                }
+            }
+        });
+
         return rootView;
+    }
+
+    private void expand() {
+        //set Visible
+        mLinearLayout.setVisibility(View.VISIBLE);
+        mAnimator.start();
+    }
+
+    private void collapse() {
+        int finalHeight = mLinearLayout.getHeight();
+
+        ValueAnimator mAnimator = slideAnimator(finalHeight, 0);
+
+        mAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                //Height=0, but it set visibility to GONE
+                mLinearLayout.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animator) {
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+            }
+        });
+        mAnimator.start();
+    }
+
+
+    private ValueAnimator slideAnimator(int start, int end) {
+
+        ValueAnimator animator = ValueAnimator.ofInt(start, end);
+
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                //Update Height
+                int value = (Integer) valueAnimator.getAnimatedValue();
+
+                ViewGroup.LayoutParams layoutParams = mLinearLayout.getLayoutParams();
+                layoutParams.height = value;
+                mLinearLayout.setLayoutParams(layoutParams);
+            }
+        });
+        return animator;
     }
 }
